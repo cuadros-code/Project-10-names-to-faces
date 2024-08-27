3a978127c2289703fc5551c1e0ac9c2e25302944
kevin cuadros <kevindavidcuadros@gmail.com>
2024-08-27 17:31:19 -0500

    Add methods to show cell

--- Project-10-names-to-faces/ViewController.swift
+++ Project-10-names-to-faces/ViewController.swift
@@ -11,7 +11,23 @@ class ViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Names to faces"
         // Do any additional setup after loading the view.
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: "Person",
+            for: indexPath
+        ) as? PersonCell else {
+            fatalError("Cell not found")
+        }
+        return cell 
     }
 
 
